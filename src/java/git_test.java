e350c60a88413d0a9eb4b7844fd0b2eb31eb77bf
Jianglai <lingxg2024@lzu.edu.cn>
2026-03-31 11:39:03 +0800

    hot-fix commit1

--- src/java/git_test.java
+++ src/java/git_test.java
@@ -2,7 +2,8 @@ package java;
 
 public class git_test {
     public static void main( String[] args) {
-        System.out.print("hello git and java");
-        System.out.print("hello git and java");
+        System.out.print("hello git and java1");
+        System.out.print("hello git and java2");
+        System.out.print("hello git and java3");
     }
 }
