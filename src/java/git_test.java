8533c6d104ee70eca23a212c7ebed74d07f32bd4
Jianglai <lingxg2024@lzu.edu.cn>
2026-03-31 11:27:51 +0800

    first commit

---
+++ src/java/git_test.java
@@ -0,0 +1,8 @@
+package java;
+
+public class git_test {
+    public static void main( String[] args) {
+        System.out.print("hello git and java");
+        System.out.print("hello git and java");
+    }
+}
